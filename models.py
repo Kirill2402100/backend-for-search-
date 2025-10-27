5d045b478b44cd43d688b02d8c6250629ef2bfdd
Kirill2402100 <miroshkin@inbox.ru>
2025-10-27 14:26:31 +0100

    Create models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+class LeadStatus:
+    NEW = "new_lead"
+    EMAIL_FOUND = "email_found"
+    EMAIL_VALID = "email_valid"
+    PROPOSAL_SENT = "proposal_sent"
+    REPLIED = "replied"
+    INVALID_EMAIL = "invalid_email"
