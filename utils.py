2ad722d5727b0ae2cbde8e57601434f1ddeb7774
Kirill2402100 <miroshkin@inbox.ru>
2025-10-31 10:32:00 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from typing import Dict, Any
 def _task_status_str(task: Dict[str, Any]) -> str:
     """
     ClickUp иногда возвращает 'status': 'open', а иногда 'status': {'status': 'open', ...}
+    Эта функция-хелпер теперь здесь, чтобы избежать циклического импорта.
     """
     st = task.get("status")
     if isinstance(st, str):
